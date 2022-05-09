d2207892f59d39ffe4509c655a1badd64a2d3bc9
infGAI <informatics444@gmail.com>
2022-05-09 14:45:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, make_response, jsonify
+from data import db_session, news_api
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.errorhandler(404)
+def not_found(_):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+def main():
+    db_session.global_init("test.db")
+    app.register_blueprint(news_api.blueprint)
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
